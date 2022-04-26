06aadce92b5aecf353eea69bf6cc97e7b8b66332
NickeaBennett <nickeabennett@gmail.com>
2022-04-26 10:32:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    targetText()
+    console.log("The DOM has loaded");
+});
+
+function targetText() {
+    document.getElementById('text');
+    text.textContent = "This is really cool!"
+}
